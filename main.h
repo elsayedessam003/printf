0e6e404bf8d360ff6a9400850956fdc78cae05bf
sameh0075 <samehelewa63@gmail.com>
2023-08-21 11:58:23 -0700

    main

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+
+
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
